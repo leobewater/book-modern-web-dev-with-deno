493486f540741924636ac021b2efbdfad9b95396
Leo <leoc2628@gmail.com>
2022-12-01 17:21:45 -0500

    update comment

--- 01-permission-flags/permission_list.ts
+++ 01-permission-flags/permission_list.ts
@@ -3,5 +3,11 @@ fetch("https://deno.land");
 fetch("https://github.com");
 fetch("https://google.com");
 
-// Run in terminal
-// > deno run --allow-net permission_list.ts
+/* Run in terminal
+
+# All domains are allowed to access the network
+> deno run --allow-net permission_list.ts”
+
+# Only ‘deno.land’ and ‘github.com’ domains are allowed network access
+> deno run --allow-net=deno.land,github.com permission_list.ts
+*/
