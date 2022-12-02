3f37bba757a80942a2d500407d53a284a446e161
Leo <leoc2628@gmail.com>
2022-12-02 16:02:39 -0500

    add cors

--- 07-phonebook/src/mod.ts
+++ 07-phonebook/src/mod.ts
@@ -1,8 +1,15 @@
 import { Application } from 'oak';
 import { GraphQLService } from './middlewares.ts';
+import {oakCors} from 'cors';
 
 const app = new Application();
 
+const _PORT = Deno.env.get(<PORT>);
+const PORT = parseInt(_PORT as string);
+const port = isNaN(PORT) ? 8080: PORT;
+
+app.use(oakCors());
 app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
-console.log('Server listening on https://localhost8080/graphql');
-await app.listen({ port: 8080 });
+
+console.log(`Server listening on ${port}graphql`);
+await app.listen({ port });
